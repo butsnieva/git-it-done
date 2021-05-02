b02bca25524cef86f5cc695704c90dd96286ae0b
Kateryna B <butsneva.ekaterina@gmail.com>
2021-05-02 00:46:55 -0500

    display response data on page

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,7 @@
 var userFormEl = document.querySelector('#user-form')
 var nameInputEl = document.querySelector('#username')
+var repoContainerEl = document.querySelector('#repos-container')
+var repoSearhTerm = document.querySelector('#repo-search-term')
 
 var getUserRepos = function (user) {
     //format the githab api url 
@@ -10,7 +12,7 @@ var getUserRepos = function (user) {
     .then(function(response) {
         response.json()
         .then(function(data){
-            console.log(data)
+            displayRepos(data, user)
         })
     }) 
 }
@@ -29,3 +31,44 @@ var formSubmitHandler = function(event) {
 }
 userFormEl.addEventListener('submit', formSubmitHandler)
 
+var displayRepos = function(repos, searchTerm) {
+    repoContainerEl.textContent = ''
+    repoSearhTerm.textContent = searchTerm
+
+    //loop over repos
+    for (var i = 0; i < repos.length; i++) {
+        //format repo name
+        var repoName = repos[i].owner.login + '/' + repos[i].name
+        
+        //create a container for each repo
+        var repoEl = document.createElement('div')
+        repoEl.classList = 'list-item flex-row justify-space-between align-center'
+
+        //create a span el to hold repo name
+        var titleEl = document.createElement('span')
+        titleEl.textContent = repoName
+
+        //append to container
+        repoEl.appendChild(titleEl)
+
+        //append container to the dom
+        repoContainerEl.appendChild(repoEl)
+
+        //create a status el
+        var statusEl = document.createElement('span')
+        statusEl.classList = 'flex-row align-center'
+
+        //check if current  repo has issues or not  
+        if (repos[i].open_issues_count > 0) {
+            statusEl.innerHTML = '<i class="fas fa-times status-icon icon-danger"></i>' + repos[i].open_issues_count + 'issue(s)'
+        } else {
+            statusEl.innerHTML = '<i class="fas fa-check-square status-icon icon-success"></i>'
+        }
+
+        //append to container 
+        repoEl.appendChild(statusEl)
+    }
+
+    console.log(repos)
+    console.log(searchTerm)
+}
