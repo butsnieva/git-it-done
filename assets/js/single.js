fb327d90299ec0caad8cd3e05fc39466124a0093
Kateryna B <butsneva.ekaterina@gmail.com>
2021-05-02 15:40:07 -0500

    error handling

--- assets/js/single.js
+++ assets/js/single.js
@@ -17,15 +17,19 @@ var getRepoIssues = function (repo) {
                 }
             })
         } else {
-            alert('There was a problem with your request!')
+            document.location.replace('./index.html')
         }
     })
 }
 var getRepoName = function(){
     var queryString = document.location.search
     var repoName = queryString.split('=')[1]
-    getRepoIssues(repoName)
-    repoNameEl.textContent = repoName
+    if (repoName) {
+        getRepoIssues(repoName)
+        repoNameEl.textContent = repoName
+    } else {
+        document.location.replace('./index.html')
+    }
 }
 getRepoName()
 
