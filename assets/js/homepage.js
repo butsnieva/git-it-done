4171dfa634b2d5be396bdddbf7edd74f860558eb
Kateryna B <butsneva.ekaterina@gmail.com>
2021-05-01 23:35:42 -0500

    handle form submission

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,6 @@
+var userFormEl = document.querySelector('#user-form')
+var nameInputEl = document.querySelector('#username')
+
 var getUserRepos = function (user) {
     //format the githab api url 
     var apiUrl = 'https://api.github.com/users/' + user + '/repos'
@@ -12,6 +15,17 @@ var getUserRepos = function (user) {
     }) 
 }
 
-getUserRepos('butsnieva')
+var formSubmitHandler = function(event) {
+    event.preventDefault()
+    //get value from input el
+    var username = nameInputEl.value.trim()
 
+    if (username) {
+        getUserRepos(username)
+        nameInputEl.value = ''
+    } else {
+        alert ('Please enter a github username')
+    }
+}
+userFormEl.addEventListener('submit', formSubmitHandler)
 
